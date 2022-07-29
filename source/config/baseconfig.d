6a0aefe7fad4ca5a20637493856a22add3602f63
dk949 <56653556+dk949@users.noreply.github.com>
2022-07-29 12:05:28 +0300

    config: added a check for unused kesy in yaml

--- source/config/baseconfig.d
+++ source/config/baseconfig.d
@@ -5,12 +5,31 @@ import config.utils;
 import std.traits;
 
 class BaseConfig {
-    protected void fromYamlImpl(This)(auto ref inout(Node) node)
+    protected void fromYamlImpl(This)(Node node)
     if (is(This : BaseConfig)) {
+        //Note: node is copied, because this function will remove elements from it
         static foreach (field; FieldNameTuple!This)
-            if (field in node)
+            if (field in node) {
                 __traits(getMember, cast(This) this, field)
                     = node[field].tryAs!(typeof(__traits(getMember, cast(This) this, field)));
+                node.removeAt(field);
+            }
+        import std.range;
+
+        import std.stdio;
+
+        if (!node.empty) {
+            debug {
+                import std.stdio;
+
+                stderr.writeln("[DEBUG] Warning: Found unknown keys in ", node.startMark.name, ":");
+                foreach (key; node.mappingKeys!string) {
+                    stderr.writeln("\t", key);
+                }
+            } else {
+                static assert(0, "Figure out how to handle warnings");
+            }
+        }
     }
 
     abstract void fromYaml(ref Node node);
